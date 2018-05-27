9817b9f351ffe604c93aa16f7f9c07911947d1c8
康永敢 <kangyonggan@gmail.com>
2018-05-27 20:05:55 +0800

    新增

---
+++ src/components/FormModal.vue
@@ -0,0 +1,47 @@
+<template>
+  <Modal :title="title" v-model="showModel" :mask-closable="false" :closable="false">
+    <slot></slot>
+  </Modal>
+</template>
+
+<script>
+  export default {
+    name: 'FormModal',
+    props: {
+      title: {
+        required: true,
+        type: String
+      }
+    },
+    data() {
+      return {
+        isLoading: false,
+        showModel: false
+      }
+    },
+    methods: {
+      show: function () {
+        this.stop();
+        this.showModel = true;
+      },
+      hide: function () {
+        this.stop();
+        this.showModel = false;
+      },
+      loading: function () {
+        this.isLoading = true;
+      },
+      stop: function () {
+        this.isLoading = false;
+      },
+      click: function () {
+        this.loading();
+        this.ok();
+      }
+    }
+  }
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+</style>
--- src/views/system/User.vue
+++ src/views/system/User.vue
@@ -1,6 +1,6 @@
 <template>
   <div>
-    <Form ref="form" :model="params" inline>
+    <Form ref="queryForm" :model="params" inline>
       <FormItem prop="username">
         <Input v-model="params.username" placeholder="请输入用户名"/>
       </FormItem>
@@ -8,16 +8,17 @@
         <Input v-model="params.realname" placeholder="请输入真实姓名"/>
       </FormItem>
       <FormItem prop="startCreatedTime">
-        <DatePicker v-model="params.startCreatedTime" placeholder="请选择创建开始日期" @on-change="params.startCreatedTime=$event"/>
+        <DatePicker v-model="params.startCreatedTime" placeholder="请选择创建开始日期"
+                    @on-change="params.startCreatedTime=$event"/>
       </FormItem>
       <FormItem prop="endCreatedTime">
         <DatePicker v-model="params.endCreatedTime" placeholder="请选择创建结束日期" @on-change="params.endCreatedTime=$event"/>
       </FormItem>
       <Row>
         <FormItem>
-          <Button type="info" icon="ios-search" @click="query($refs.form)">查询</Button>
-          <Button type="warning" icon="ios-refresh-empty" @click="reset($refs.form)">清除</Button>
-          <Button type="primary" icon="plus">新增</Button>
+          <Button type="info" icon="ios-search" @click="query($refs.queryForm)">查询</Button>
+          <Button type="warning" icon="ios-refresh-empty" @click="reset($refs.queryForm)">清除</Button>
+          <Button type="primary" icon="plus" @click="$refs.userModal.show()">新增</Button>
           <Button type="error" icon="trash-a" @click="$refs.deleteConfirm.show()">删除</Button>
           <Button type="warning" icon="ios-undo" @click="$refs.recoveryConfirm.show()">恢复</Button>
         </FormItem>
@@ -27,25 +28,47 @@
     <Table border :columns="columns" :data="pageInfo.list" @on-selection-change="selectionChange"/>
 
     <Page :total="pageInfo.total" show-total show-sizer show-elevator :style="{marginTop: '20px'}"
-          @on-change="jump($event, $refs.form)" @on-page-size-change="changePageSize($event, $refs.form)"></Page>
+          @on-change="jump($event, $refs.queryForm)"
+          @on-page-size-change="changePageSize($event, $refs.queryForm)"></Page>
 
-    <Confirm ref="deleteConfirm" title="删除确认" content="此次删除为逻辑删除，可以对已删除的数据进行恢复！" btnText="确认删除" type="error" :ok="deleteBatch"/>
+    <Confirm ref="deleteConfirm" title="删除确认" content="此次删除为逻辑删除，可以对已删除的数据进行恢复！" btnText="确认删除" type="error"
+             :ok="deleteBatch"/>
     <Confirm ref="recoveryConfirm" title="恢复确认" content="此次恢复为逻辑恢复，可以对已恢复的数据进行删除！" btnText="确认恢复" :ok="recoveryBatch"/>
+
+    <FormModal ref="userModal" title="新增用户">
+
+      <Form ref="form" :model="user" :rules="userValidate">
+        <FormItem label="用户名" prop="username">
+          <Input v-model="user.username" placeholder="请输入用户名"/>
+        </FormItem>
+        <FormItem label="真实姓名" prop="realname">
+          <Input v-model="user.realname" placeholder="请输入真实姓名"/>
+        </FormItem>
+        <FormItem label="密码" prop="password">
+          <Input type="password" v-model="user.password" placeholder="请输入密码"/>
+        </FormItem>
+      </Form>
+    </FormModal>
   </div>
 </template>
 
 <script>
   import Confirm from '@/components/Confirm';
+  import FormModal from "../../components/FormModal";
 
   export default {
     name: 'User',
-    components: {Confirm},
+    components: {FormModal, Confirm},
     data() {
       return {
         /**
          * 搜索表单的参数
          */
         params: {},
+        /**
+         * 模态框表单的参数
+         */
+        user: {},
         /**
          * 分页信息
          */
@@ -124,20 +147,34 @@
                 }, '设置密码')
               ]);
             }
-          }]
+          }],
+        /**
+         * 表单的校验
+         */
+        userValidate: {
+          username: [
+            {required: true, message: '用户名为必填项'}
+          ],
+          realname: [
+            {required: true, message: '真实姓名为必填项'}
+          ],
+          password: [
+            {required: true, message: '密码为必填项'}
+          ]
+        }
       }
     },
     mounted: function () {
       /**
        * 初始化完成后发起查询
        */
-      this.query(this.$refs.form);
+      this.query(this.$refs.queryForm);
     },
     methods: {
       /**
        * 批量删除
        */
-      deleteBatch: function() {
+      deleteBatch: function () {
         let usernames = [];
         for (let i = 0; i < this.selection.length; i++) {
           usernames.push(this.selection[i].username);
@@ -154,14 +191,14 @@
           "usernames": usernames.join(",")
         }, function (data) {
           that.$Message.success(data.respMsg);
-          that.query(that.$refs.form);
+          that.query(that.$refs.queryForm);
         });
         that.$refs.deleteConfirm.hide();
       },
       /**
        * 批量恢复
        */
-      recoveryBatch: function() {
+      recoveryBatch: function () {
         let usernames = [];
         for (let i = 0; i < this.selection.length; i++) {
           usernames.push(this.selection[i].username);
@@ -178,7 +215,7 @@
           "usernames": usernames.join(",")
         }, function (data) {
           that.$Message.success(data.respMsg);
-          that.query(that.$refs.form);
+          that.query(that.$refs.queryForm);
         });
         that.$refs.recoveryConfirm.hide();
       },
